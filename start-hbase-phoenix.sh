1debdcfa8402d5588e318ec7562418e756a1b571
Francis Chuang <francis.chuang@boostport.com>
2016-08-19 15:19:05 +1000

    Remove configuration that is not needed in phoenix 4.8.0.

--- start-hbase-phoenix.sh
+++ start-hbase-phoenix.sh
@@ -17,12 +17,9 @@ addConfig () {
      $1
 }
 
-addConfig $HBASE_SITE "hbase.master.loadbalancer.class" "org.apache.phoenix.hbase.index.balancer.IndexLoadBalancer"
-addConfig $HBASE_SITE "hbase.coprocessor.master.classes" "org.apache.phoenix.hbase.index.master.IndexMasterObserver"
 addConfig $HBASE_SITE "hbase.regionserver.wal.codec" "org.apache.hadoop.hbase.regionserver.wal.IndexedWALEditCodec"
 addConfig $HBASE_SITE "hbase.region.server.rpc.scheduler.factory.class" "org.apache.hadoop.hbase.ipc.PhoenixRpcSchedulerFactory"
 addConfig $HBASE_SITE "hbase.rpc.controllerfactory.class" "org.apache.hadoop.hbase.ipc.controller.ServerRpcControllerFactory"
-addConfig $HBASE_SITE "hbase.coprocessor.regionserver.classes" "org.apache.hadoop.hbase.regionserver.LocalIndexMerger"
 addConfig $HBASE_SITE "data.tx.snapshot.dir" "/tmp/tephra/snapshots"
 addConfig $HBASE_SITE "data.tx.timeout" "60"
 addConfig $HBASE_SITE "phoenix.transactions.enabled" true
